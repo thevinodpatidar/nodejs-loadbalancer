150d447f92c9405392ee23d317924d860c326ca8
Vinod Patidar <pvinod813@gmail.com>
2021-09-17 13:23:14 +0530

    Basic Worker Nodejs Cluster Example

--- index.js
+++ index.js
@@ -3,30 +3,46 @@ const app = express();
 const port = process.env.PORT || 4000;
 const cluster = require('cluster');
 const numOfCPU = require('os').cpus().length;
-const fabObject = require('./math-logic/fibonacci-series');
 
 if (cluster.isMaster) {
-    console.log(`Primary ${process.pid} is running`);
 
-    // Fork workers.
+    console.log(`Master Process Id is - ${process.pid}`);
+
+    const worker1 = require("child_process").fork("./workers/fab-series-worker1");
+    const worker2 = require("child_process").fork("./workers/fab-series-worker2");
+
+    console.log(`Child Process ID is ${worker1.pid}`);
+    console.log(`Child Process ID is ${worker2.pid}`);
+
+    worker1.on('message', function (number) {
+        // Receive results from child process - 1
+        console.log(`Fab Number from Child Process - 1 is ${number}`);
+    });
+    worker2.on('message', function (number) {
+        // Receive results from child process - 2
+        console.log(`Fab Number from Child Process - 2 is ${number}`);
+    });
+    cluster.on("online", worker => {
+        console.log(`Message received from - ${worker.process.pid}`)
+        worker.on("message", num => {
+            if (num % 2 === 0) {
+                worker1.send(num);
+            } else {
+                worker2.send(num);
+            }
+        });
+    });
     for (let i = 0; i < numOfCPU - 2; i++) {
-        cluster.fork();
+        let worker = cluster.fork();
+        console.log(`Worker started on PID - ${worker.process.pid}`);
     }
-
-    cluster.on('online', (worker) => {
-        console.log(`Worker PID - ${worker.process.pid}`);
-    });
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
-
+    console.log(`Total Number of CPU Count is ${numOfCPU}`);
 } else {
-    app.get('/', (req, res) => {
-        console.log(`Worker Process ID - ${cluster.worker.process.pid} has accepted the request`);
-        const num = Number.parseInt(req.query.number);
-        const result = fabObject.calculateFibonacciValue(num);
-        res.send(`<h1>Result - ${result}</h1>`)
+    app.get("/", (request, response) => {
+        process.send(request.query.number);
+        console.log(`Process Id ${process.pid} received the request!`);
+        response.send("<h3>The request has been received successfully! We will send an email once your calculation is ready!</h3>");
+        response.end();
     });
     
     app.listen(port, () => console.log(`listening on http://localhost:${port}`));
