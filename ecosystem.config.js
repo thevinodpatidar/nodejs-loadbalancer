7f04b098737249b2c3cabdfc5d59c9535196bf27
Vinod Patidar <pvinod813@gmail.com>
2021-09-17 14:30:34 +0530

    PM2 Worker with redis PubSub example

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,23 @@
 module.exports = {
-  apps : [{
-    name: 'EXPRESS REST API',
-    script: 'index.js',
-    instances: 'MAX',
-    autorestart: true,
-    exec_mode:"cluster",
-    watch: true,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: 'Publisher Application',
+      script: 'index.js',
+      instances: 6,
+      autorestart: true,
+      exec_mode: "cluster",
+      watch: true,
+      max_memory_restart: '1G'
     },
-    env_production: {
-      NODE_ENV: 'production'
+    {
+      name: 'Subscriber-1',
+      script: 'subscribers/subscriber-worker1.js',
+      instances: 1
+    },
+    {
+      name: 'Subscriber-2',
+      script: 'subscribers/subscriber-worker2.js',
+      instances: 1
     }
-  }]
+  ]
 };
--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const jphRoutes = require('./jph-routes');
+const redis = require("redis");
+const keys = require("./keys");
 
-app.use("/jph", jphRoutes);
+let client = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort
+});
+
+app.get("/", (request, response) => {
+    console.log(`Process ID is ${process.pid}`);
+    let num = request.query.number;
+    if (num % 2 === 0) {
+        client.publish("math-subscription1", num);
+    } else {
+        client.publish("math-subscription2", num);
+    }
+    response.end("<h3>Notification sent to the respective subscribers! We will send you an email with the details!</h3>");
+});
+
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
 
